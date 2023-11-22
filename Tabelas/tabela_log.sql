7a03dc7e666500d5ef034ad539ad589d092cb470
Gabriela Mariano <gabrielamariano@live.com>
2023-11-22 08:25:09 -0300

    Corrige sintaxe do SQL

--- Tabelas/tabela_log.sql
+++ Tabelas/tabela_log.sql
@@ -1,22 +1,33 @@
 CREATE TABLE log_users (
-  id_log INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  senha text,
-  data_nascimento DATE,
-  data_delecao datetime DEFAULT CURRENT_TIMESTAMP,
-  iduser INT,
-	FOREIGN KEY(iduser) 
-	REFERENCES users(id)
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	senha text,
+	datanascimento DATE,
+	data_delecao DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+
 CREATE TABLE log_produto (
-  id_log INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nome VARCHAR(255) NOT NULL,
   preco INT NOT NULL,
   quantidade INT,
-  data_delecao datetime DEFAULT CURRENT_TIMESTAMP,
-  idprod INT,
-	FOREIGN KEY (idprod)
-    REFERENCES produtos (id)
+  data_delecao datetime DEFAULT CURRENT_TIMESTAMP
 );
+
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` TRIGGER `enviar_log` AFTER DELETE ON `users` FOR EACH ROW BEGIN
+    INSERT INTO log_users (nome, email, senha, datanascimento, data_delecao)
+    VALUES (OLD.nome, OLD.email, OLD.senha, OLD.datanascimento, NOW());
+END
+//
+DELIMITER ;
+
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` TRIGGER `enviar_log_produto` AFTER DELETE ON `produtos` FOR EACH ROW BEGIN
+    INSERT INTO log_produto (nome, preco, quantidade, data_delecao)
+    VALUES (OLD.nome, OLD.preco, OLD.quantidade, NOW());
+END
+//
+DELIMITER ;
